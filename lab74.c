0caffda30470a543b002ee49f3aa343c0297731c
6704062610089 <s6704062610089@email.kmutnb.ac.th>
2026-02-07 18:45:06 +0700

    Lab7no4.c: Square of stars

---
+++ lab74.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void printHead(int w);
+void printBody(int w, int h);
+
+void main() {
+	int width,height;
+	scanf("%d %d",&width,&height);
+	printHead(width);
+	printBody(width,height);
+	printHead(width);
+}
+
+void printHead(int w){
+	for(int i  = 0 ;i < w;i++){
+		if(i==w-1){
+			printf("*");
+		}else{
+			printf("* ");
+		}
+	}
+	printf("\n");
+}
+void printBody(int w, int h){
+	for(int j = 0;j<h-2;j++){
+	for(int i  =0;i<w;i++){
+		if(i==0||i==w-1){
+			printf("* ");
+		}else{
+			printf("  ");
+		}
+	}
+	printf("\n");
+}
+}
+
